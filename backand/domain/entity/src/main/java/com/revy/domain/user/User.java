d9337d1eff7f92a8368a00d1f58f9c969f281508
Revy <wantedhooni@gmail.com>
2026-02-23 03:39:29 +0900

    feat(backand/domain/entity/src/main/java/com/revy/domain/user/User.java): User.java의 fields, methods, constructor, static method 모두 바뀐 것으로 보임.
    다음은 Conventional Commit 메시지입니다.
    
    ```
    feat(user): entity 및 repository 수정
    ```

--- backand/domain/entity/src/main/java/com/revy/domain/user/User.java
+++ backand/domain/entity/src/main/java/com/revy/domain/user/User.java
@@ -1,31 +1,45 @@
 package com.revy.domain.user;
 
-import com.revy.domain.admin.enums.AdminStatus;
 import com.revy.domain.base.BaseEntity;
+import com.revy.domain.user.enums.UserStatus;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(name = "user")
 @Getter
-@NoArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class User extends BaseEntity {
-    @Column(name="email", unique = true, nullable = false)
+    @Column(name = "email", unique = true, nullable = false)
     private String email;
 
-    @Column(name="password", nullable = false)
+    @Column(name = "password", nullable = false)
     private String password;
 
-    @Column(name="status", nullable = false)
-    @Enumerated(EnumType.STRING)
-    private AdminStatus status;
+    @Column(name = "first_name")
+    private String firstName;
 
-    @Column(name="enabled")
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "nick_name")
+    private String nickName;
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    private UserStatus status;
+
+    @Column(name = "fail_count")
+    private int failCount;
+
+    @Column(name = "enabled")
     private boolean enabled;
 
     public void changeEmail(String email) {
@@ -36,12 +50,41 @@ public class User extends BaseEntity {
         this.password = password;
     }
 
-    public void changeStatus(AdminStatus status) {
-        this.status = status;
-    }
-
     public void changeEnabled(boolean enabled) {
         this.enabled = enabled;
     }
 
+    public void increaseFailCount() {
+        this.failCount++;
+    }
+
+    public void resetFailCount() {
+        this.failCount = 0;
+    }
+
+    @Builder
+    private User(String email, String password, String firstName, String lastName, String nickName, UserStatus status,
+                 int failCount, boolean enabled) {
+        this.email = email;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.nickName = nickName;
+        this.status = status;
+        this.failCount = failCount;
+        this.enabled = enabled;
+    }
+
+    public static User createUser(String email, String password, String firstName, String lastName, String nickName) {
+        return User.builder()
+                   .email(email)
+                   .password(password)
+                   .firstName(firstName)
+                   .lastName(lastName)
+                   .nickName(nickName)
+                   .status(UserStatus.ACTIVE)
+                   .failCount(0)
+                   .enabled(true)
+                   .build();
+    }
 }
