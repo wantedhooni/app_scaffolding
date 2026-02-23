b666c27474cebe68aa8851c953e3d6d478399be4
Revy <wantedhooni@gmail.com>
2026-02-24 01:14:07 +0900

    feat(domain/entity-base/src/main/java/com/revy/domain/base/BaseRepository.java): Spring Data Repository 구체화 ( CrudRepository 추가 )

--- backand/domain/entity-base/src/main/java/com/revy/domain/base/BaseRepository.java
+++ backand/domain/entity-base/src/main/java/com/revy/domain/base/BaseRepository.java
@@ -2,10 +2,10 @@ package com.revy.domain.base;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
 
 @NoRepositoryBean
-public interface BaseRepository<T, ID> extends JpaRepository<T, ID>,
+public interface BaseRepository<T, ID> extends JpaRepository<T, ID>, CrudRepository<T, ID>,
         QuerydslPredicateExecutor<T> {
-
 }
